4a88ced063a51abc7970a8bce985f8f10228f6c4
Walle10-0 <lukemauro900@gmail.com>
2025-12-03 13:38:53 -0500

    missing import

--- vpn/VPNtools.h
+++ vpn/VPNtools.h
@@ -1,6 +1,8 @@
 #ifndef VPNTOOLS_H
 #define VPNTOOLS_H
 
+#include <netinet/in.h>
+
 #define TUNTAP_NAME "vpnclient"
 #define VPN_CLIENT_IP "10.8.0.2/24"
 #define VPN_PRIVATE_SERVER_IP "10.8.0.1/24"
